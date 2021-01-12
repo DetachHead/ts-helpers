181a37b031ead9cd4735c595cd1c69dffd19ac36
DetachHead <detachhead@users.noreply.github.com>
2021-01-13 05:46:06 +1000

    better formatting for the PrependNextNum & clean up EnumerateInternal

--- src/utilityTypes.ts
+++ src/utilityTypes.ts
@@ -1,7 +1,13 @@
 import {Primitive} from 'utility-types'
 
-type PrependNextNum<A extends Array<unknown>> = A['length'] extends infer T ? ((t: T, ...a: A) => void) extends ((...x: infer X) => void) ? X : never : never;
-type EnumerateInternal<A extends Array<unknown>, N extends number> = { 0: A, 1: EnumerateInternal<PrependNextNum<A>, N> }[N extends A['length'] ? 0 : 1];
+type PrependNextNum<A extends Array<unknown>> = A['length'] extends infer T
+    ? ((t: T, ...a: A) => void) extends ((...x: infer X) => void)
+        ? X
+        : never
+    : never;
+
+type EnumerateInternal<A extends Array<unknown>, N extends number> =
+    N extends A['length'] ? A : EnumerateInternal<PrependNextNum<A>, N>
 
 /**
  * creates a union type of numbers from 0 to generic `N`
