0ba112f67ee4b1a0f0bee27447bb24fc986123b9
DetachHead <detachhead@users.noreply.github.com>
2021-01-25 00:01:39 +1000

    fix formatting with new line width

--- src/types/Number.ts
+++ src/types/Number.ts
@@ -5,6 +5,9 @@ import { Range } from '../utilityTypes'
  * gets a random number in the given range from `Min` to `Max` (inclusive)
  * @returns anumber where the type is a union of the possible numbers
  */
-export function random<Min extends number, Max extends number>(min: Min, max: Max): Range<Min, Max> {
+export function random<Min extends number, Max extends number>(
+	min: Min,
+	max: Max
+): Range<Min, Max> {
 	return _.random(min, max) as Range<Min, Max>
 }
