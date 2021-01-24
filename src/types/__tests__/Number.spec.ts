9b327d2c068b187c279142e5b4d1371618d5cbe8
DetachHead <detachhead@users.noreply.github.com>
2021-01-24 23:04:40 +1000

    BadExpressionStatementJS for random test

--- src/types/__tests__/Number.spec.ts
+++ src/types/__tests__/Number.spec.ts
@@ -4,5 +4,6 @@ import { TupleOf } from 'utility-types'
 test('random', () => {
 	const arr: TupleOf<number, 5> = [1, 2, 3, 4, 5]
 	const foo = random(1, 4)
+	// noinspection BadExpressionStatementJS
 	arr[foo] // $ExpectType number
 })
