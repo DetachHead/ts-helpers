f6060be9932a21a25566be989408834019d31bcc
DetachHead <detachhead@users.noreply.github.com>
2021-01-26 02:05:56 +1000

    add TupleOfExcluding & TupleOfUpToButNotIncluding types and fix TupleOfUpTo

--- src/utilityTypes.ts
+++ src/utilityTypes.ts
@@ -20,8 +20,30 @@ export type NoUncheckedIndexedAccess = undefined extends typeof indexedAccessChe
  * foo[0] //number (or number|undefined if `noUncheckedIndexedAccess` is enabled)
  * foo[3] //error: tuple of length '3' has no element at index '3'
  */
-//TODO: figure out a more accurate way of doing this, as (T|undefined)[] isn't the exact same as T[] with noUncheckedIndexedAccess
-export type TupleOfUpTo<T, L extends number> = TupleOf<NoUncheckedIndexedAccess extends true ? T | undefined : T, L>
+export type TupleOfUpTo<T, L extends number> = TupleOf<T, L> | (NoUncheckedIndexedAccess extends true ? [] : never)
+
+/**
+ * an array of length `L` - 1
+ * @example
+ * declare const foo: TupleOfExcluding<number, 3>
+ * foo[1] //number
+ * foo[2] //error: tuple of length '2' has no element at index '2'
+ */
+//never doesnt work for infers
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type TupleOfExcluding<T, L extends number> = TupleOf<T, L> extends [any, ...infer R] ? R : never
+
+
+/**
+ * an array that can be of any length between 0 and `L`, excluding `L`
+ * @example
+ * declare const foo: TupleOfUpToButNotIncluding<number, 3>
+ * foo[1] //number (or number|undefined if `noUncheckedIndexedAccess` is enabled)
+ * foo[2] //error: tuple of length '2' has no element at index '2'
+ */
+export type TupleOfUpToButNotIncluding<T, L extends number> =
+	TupleOfExcluding<T, L>
+	| (NoUncheckedIndexedAccess extends true ? [] : never)
 
 type PrependNextNum<A extends Array<unknown>> = A['length'] extends infer T
 	? ((t: T, ...a: A) => void) extends (...x: infer X) => void
