ea6f32404d20fb6a6d37321b5f2b068a2b04fc64
DetachHead <DetachHead@users.noreply.github.com>
2021-02-24 22:55:17 +1000

    add some string types

--- src/utilityTypes/__tests__/String.spec.ts
+++ src/utilityTypes/__tests__/String.spec.ts
@@ -1,4 +1,4 @@
-import { UriString, UrlString } from '../String'
+import { Email, IP, UriString, UrlString } from '../String'
 import { testType } from '../../utilityFunctions/misc'
 
 test('UriString', () => {
@@ -12,4 +12,17 @@ test('UrlString', () => {
 	//@ts-expect-error TS2345: Argument of type '"asdf"' is not assignable to parameter of type '`${string}://${string}`'.
 	testType<UrlString>('asdf')
 	testType<UrlString>('https://asdf.com')
+})
+test('Email', () => {
+	//@ts-expect-error TS2345: Argument of type '"asdf.com"' is not assignable to parameter of type '`${string}@${string}.${string}`'.
+	testType<Email>('asdf.com')
+	//@ts-expect-error TS2345: Argument of type '"foo@asdf"' is not assignable to parameter of type '`${string}@${string}.${string}`'.
+	testType<Email>('foo@asdf')
+	testType<Email>('foo@bar.com')
+})
+
+test('IP', () => {
+	//@ts-expect-error invalid ip
+	testType<IP>('a.b.c.d')
+	testType<IP>('192.168.0.1')
 })
