27822599bcb92b3d3cb75a4aacf6b01f93f61a0d
DetachHead <detachhead@users.noreply.github.com>
2021-03-08 20:24:15 +1000

    don't use any, any is cringe

--- src/utilityTypes/Array.ts
+++ src/utilityTypes/Array.ts
@@ -37,7 +37,7 @@ type _ConcatLargestUntilDone<
 			[...AccumulatedArray[0], ...NextArray]
 	  >
 
-type _Replace<R extends any[], T> = { [K in keyof R]: T }
+type _Replace<R extends unknown[], T> = { [K in keyof R]: T }
 
 /**
  * creates an array with a fixed length
