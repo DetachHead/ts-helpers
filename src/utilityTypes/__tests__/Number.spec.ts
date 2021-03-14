5df067096711843692e9547132908f455a6ccf25
DetachHead <DetachHead@users.noreply.github.com>
2021-03-14 11:47:25 +1000

    add & subtract tests for distributive unions

--- src/utilityTypes/__tests__/Number.spec.ts
+++ src/utilityTypes/__tests__/Number.spec.ts
@@ -1,4 +1,4 @@
-import { Multiply } from '../Number'
+import { Add, Multiply, Subtract } from '../Number'
 import { testType } from '../../utilityFunctions/misc'
 
 test('multiply', () => {
@@ -10,4 +10,22 @@ test('multiply', () => {
 	testType<Multiply<2 | 3, 5 | 2>>(11)
 	//@ts-expect-error not valid product
 	testType<Multiply<2 | 3, 5 | 2>>(8)
+})
+
+test('add', () => {
+	testType<Add<2 | 5, 10 | 20>>(12)
+	testType<Add<2 | 5, 10 | 20>>(15)
+	testType<Add<2 | 5, 10 | 20>>(22)
+	testType<Add<2 | 5, 10 | 20>>(25)
+	//@ts-expect-error not valid sum
+	testType<Add<2 | 5, 10 | 20>>(20)
+})
+
+test('subtract', () => {
+	testType<Subtract<10 | 20, 2 | 3>>(8)
+	testType<Subtract<10 | 20, 2 | 3>>(18)
+	testType<Subtract<10 | 20, 2 | 3>>(7)
+	testType<Subtract<10 | 20, 2 | 3>>(17)
+	//@ts-expect-error not valid
+	testType<Subtract<10 | 20, 2 | 3>>(16)
 })
