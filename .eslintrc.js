532bf13a9922167bec57d3b46057507cbed21824
DetachHead <detachhead@users.noreply.github.com>
2021-04-06 21:47:34 +1000

    make eslint run prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,45 +1,30 @@
 module.exports = {
-	'env': {
-		'node': true,
-		'es2021': true,
-		'jest': true,
+	env: {
+		node: true,
+		es2021: true,
+		jest: true,
 	},
-	'extends': [
+	extends: [
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
 		'plugin:eslint-plugin-expect-type/recommended',
 		'prettier',
 	],
-	'parser': '@typescript-eslint/parser',
-	'parserOptions': {
-		'ecmaVersion': 12,
-		'sourceType': 'module',
-		'project': './tsconfig.json',
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 12,
+		sourceType: 'module',
+		project: './tsconfig.json',
 	},
-	'plugins': [
-		'@typescript-eslint',
-		'eslint-plugin-expect-type',
-	],
-	'rules': {
-		'indent': [
-			'error',
-			'tab',
-		],
-		'linebreak-style': [
-			'error',
-			'unix',
-		],
-		'quotes': [
-			'error',
-			'single',
-			{ 'avoidEscape': true },
-		],
-		'semi': [
-			'error',
-			'never',
-		],
+	plugins: ['@typescript-eslint', 'eslint-plugin-expect-type', 'prettier'],
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single', { avoidEscape: true }],
+		semi: ['error', 'never'],
 		'expect-type/expect': 'error',
 		'@typescript-eslint/ban-types': 'off',
-		'spaced-comment': 'error'
+		'spaced-comment': 'error',
+		'prettier/prettier': 'error',
 	},
 }
