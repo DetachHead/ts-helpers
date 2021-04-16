9e313043081b9c0b73ef080ec909061373090b83
DetachHead <detachhead@users.noreply.github.com>
2021-04-17 01:28:55 +1000

    TupleOfAtLeast type maybe idk i'm tired

--- src/utilityTypes/dates.ts
+++ src/utilityTypes/dates.ts
@@ -15,54 +15,55 @@ type Quarter = RangeType<1, 4>
 
 type OrdinalQuarter = Ordinal<Quarter>
 
+type FormatMap = [
+	// Era
+	['GGGGG', 'A' | 'B'],
+	['GGGG', 'Anno Domini' | 'Before Christ'],
+	['GGG', ShortEra],
+	['GG', ShortEra],
+	['G', ShortEra],
+
+	// Calendar year:
+	['yyyy', LongYear],
+	['yyy', MediumYear],
+	['yy', ShortYear],
+	['yo', OrdinalYear],
+	['y', number],
+
+	// Local week-numbering year
+	['YYYY', LongYear],
+	['YYY', MediumYear],
+	['YY', ShortYear],
+	['Yo', OrdinalYear],
+	['Y', number],
+
+	// ISO week-numbering year
+	['RRRR', LongYear],
+	['RRR', MediumYear],
+	['RR', LeadingZeros<number, 2>],
+	['R', number],
+
+	// Extended year
+	['uuuu', LongYear],
+	['uuu', MediumYear],
+	['uu', LeadingZeros<number, 2>],
+	['u', number],
+
+	// Quarter
+	['qqqq' | 'QQQQ', `${OrdinalQuarter} quarter`],
+	['qqq' | 'QQQ', `Q${Quarter}`],
+	['qq' | 'QQ', LeadingZeros<Quarter, 2>],
+	['q' | 'Q', Quarter]
+
+	// TODO: finish this
+	// BIG PROBLEMS to solve:
+	// - this array can't be expanded beyond 35 entries before it gives up due to stack depth error
+	// - ReplaceValuesWithMap replaces values that were the result of a previous replacement... probably need to implement a lexer instead
+]
+
 /**
  * creates a type for a formatted string as per the [`date-fns` format](https://date-fns.org/v2.20.1/docs/format)
  */
-export type StringifiedDate<Format extends string> = ReplaceValuesWithMap<
-	Format,
-	[
-		// Era
-		['GGGGG', 'A' | 'B'],
-		['GGGG', 'Anno Domini' | 'Before Christ'],
-		['GGG', ShortEra],
-		['GG', ShortEra],
-		['G', ShortEra],
+export type StringifiedDate<Format extends string> = ReplaceValuesWithMap<Format, FormatMap>
 
-		// Calendar year:
-		['yyyy', LongYear],
-		['yyy', MediumYear],
-		['yy', ShortYear],
-		['yo', OrdinalYear],
-		['y', number],
-
-		// Local week-numbering year
-		['YYYY', LongYear],
-		['YYY', MediumYear],
-		['YY', ShortYear],
-		['Yo', OrdinalYear],
-		['Y', number],
-
-		// ISO week-numbering year
-		['RRRR', LongYear],
-		['RRR', MediumYear],
-		['RR', LeadingZeros<number, 2>],
-		['R', number],
-
-		// Extended year
-		['uuuu', LongYear],
-		['uuu', MediumYear],
-		['uu', LeadingZeros<number, 2>],
-		['u', number],
-
-		// Quarter
-		['qqqq' | 'QQQQ', `${OrdinalQuarter} quarter`],
-		['qqq' | 'QQQ', `Q${Quarter}`],
-		['qq' | 'QQ', LeadingZeros<Quarter, 2>],
-		['q' | 'Q', Quarter]
-
-		// TODO: finish this
-		// BIG PROBLEMS to solve:
-		// - this array can't be expanded beyond 35 entries before it gives up due to stack depth error
-		// - ReplaceValuesWithMap replaces values that were the result of a previous replacement... probably need to implement a lexer instead
-	]
->
+const Foo: StringifiedDate<'YY/QQQQ'> = 'asdf'
