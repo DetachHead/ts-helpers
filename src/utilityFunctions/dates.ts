5f15b7e2f812c73c60d60208a06b7dff8fb0719d
DetachHead <detachhead@users.noreply.github.com>
2021-04-20 05:14:49 +1000

    finished date format type

---
+++ src/utilityFunctions/dates.ts
@@ -0,0 +1,10 @@
+import { FormattedDate } from '../utilityTypes/dates'
+import { format } from 'date-fns'
+
+/**
+ * formats a date using the [`date-fns` format](https://date-fns.org/v2.20.1/docs/format) function, and uses
+ * {@link FormattedDate} to generate a type for the given `formatString`
+ */
+export function formatDate<T extends string>(date: Date, formatString: T): FormattedDate<T> {
+	return format(date, formatString) as never
+}
