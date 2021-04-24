d4f162da686023a6267f93f2b2f782d341f6652d
DetachHead <detachhead@users.noreply.github.com>
2021-04-24 19:33:19 +1000

    add entries type & function, rename Object file to Any because it technically works on Any

---
+++ src/utilityFunctions/__tests__/Any.spec.ts
@@ -0,0 +1,12 @@
+import { entries } from '../Any'
+import { PowerAssert } from 'typed-nodejs-assert'
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const assert: PowerAssert = require('power-assert')
+
+test('entries', () => {
+	const value = entries({ foo: 1, bar: 'baz' }) // $ExpectType ["foo" | "bar", string | number][]
+	assert.deepStrictEqual(value, [
+		['foo', 1],
+		['bar', 'baz'],
+	])
+})
