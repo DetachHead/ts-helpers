2f90178069622795c2623b6537add8e2fd1d44ff
DetachHead <detachhead@users.noreply.github.com>
2021-07-03 18:50:57 +1000

    add misc.spec.ts and move stuff that was incorrectly in String.spec.ts

---
+++ src/utilityFunctions/__tests__/misc.spec.ts
@@ -0,0 +1,18 @@
+import isCI from 'is-ci'
+import { exactly, failCI } from '../misc'
+import { PowerAssert } from 'typed-nodejs-assert'
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const assert: PowerAssert = require('power-assert')
+
+test('exactly', () => {
+  const exactlyNumber = exactly<number>()
+  exactlyNumber(1 as number) // $ExpectType number
+  // @ts-expect-error type isn't exactly number
+  exactlyNumber(1) // $ExpectType never
+})
+
+test('failCI', () => {
+  console.log(isCI)
+  if (isCI) assert.throws(failCI)
+  else assert.doesNotThrow(failCI)
+})
