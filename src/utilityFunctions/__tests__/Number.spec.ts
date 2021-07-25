1ffaa7dae430c704002a3f73a76445ddccac9d31
DetachHead <detachhead@users.noreply.github.com>
2021-07-26 00:55:21 +1000

    2 width tabs???? what??????

--- src/utilityFunctions/__tests__/Number.spec.ts
+++ src/utilityFunctions/__tests__/Number.spec.ts
@@ -1,15 +1,15 @@
 import {
-  add,
-  random,
-  subtract,
-  multiply,
-  divide,
-  leadingZeros,
-  ordinalNumber,
-  isGreaterThan,
-  power,
-  leftShift,
-  rightShift,
+    add,
+    random,
+    subtract,
+    multiply,
+    divide,
+    leadingZeros,
+    ordinalNumber,
+    isGreaterThan,
+    power,
+    leftShift,
+    rightShift,
 } from '../Number'
 import { PowerAssert } from 'typed-nodejs-assert'
 import { TupleOf } from '../../utilityTypes/Array'
@@ -17,147 +17,147 @@ import { TupleOf } from '../../utilityTypes/Array'
 const assert: PowerAssert = require('power-assert')
 
 test('random', () => {
-  const arr: TupleOf<number, 5> = [1, 2, 3, 4, 5]
-  const foo = random(1, 4)
-  // noinspection BadExpressionStatementJS
-  arr[foo] // $ExpectType number
+    const arr: TupleOf<number, 5> = [1, 2, 3, 4, 5]
+    const foo = random(1, 4)
+    // noinspection BadExpressionStatementJS
+    arr[foo] // $ExpectType number
 })
 
 describe('arithmetic', () => {
-  describe('add', () => {
-    test('normal numbers', () => {
-      const num = add(1, 3) // $ExpectType 4
-      assert(num === 4)
+    describe('add', () => {
+        test('normal numbers', () => {
+            const num = add(1, 3) // $ExpectType 4
+            assert(num === 4)
+        })
+        test('big numbers', () => {
+            const num = add(1000, 3000) // $ExpectType 4000
+            assert(num === 4000)
+        })
     })
-    test('big numbers', () => {
-      const num = add(1000, 3000) // $ExpectType 4000
-      assert(num === 4000)
+    describe('subtract', () => {
+        test('normal numbers', () => {
+            const num = subtract(5, 3) // $ExpectType 2
+            assert(num === 2)
+        })
+        test('big numbers', () => {
+            const num = subtract(5000, 3000) // $ExpectType 2000
+            assert(num === 2000)
+        })
     })
-  })
-  describe('subtract', () => {
-    test('normal numbers', () => {
-      const num = subtract(5, 3) // $ExpectType 2
-      assert(num === 2)
+    describe('multiply', () => {
+        test('normal numbers', () => {
+            const num = multiply(5, 3) // $ExpectType 15
+            assert(num === 15)
+        })
     })
-    test('big numbers', () => {
-      const num = subtract(5000, 3000) // $ExpectType 2000
-      assert(num === 2000)
+    describe('divide', () => {
+        test('normal numbers', () => {
+            const num = divide(10, 2) // $ExpectType 5
+            assert(num === 5)
+        })
+        test('value not known at compile-time', () => {
+            const num = divide(10 as number, 2) // $ExpectType number
+            assert(num === 5)
+        })
     })
-  })
-  describe('multiply', () => {
-    test('normal numbers', () => {
-      const num = multiply(5, 3) // $ExpectType 15
-      assert(num === 15)
+    describe('power', () => {
+        test('value known at compiletime', () => {
+            const value = power(7, 4) // $ExpectType 2401
+            assert(value === 2401)
+        })
+        describe('value not known at compiletime', () => {
+            test('num', () => {
+                const value = power(2 as number, 4) // $ExpectType number
+                assert(value === 16)
+            })
+            test('power', () => {
+                const value = power(4, 8 as number) // $ExpectType number
+                assert(value === 65536)
+            })
+        })
     })
-  })
-  describe('divide', () => {
-    test('normal numbers', () => {
-      const num = divide(10, 2) // $ExpectType 5
-      assert(num === 5)
-    })
-    test('value not known at compile-time', () => {
-      const num = divide(10 as number, 2) // $ExpectType number
-      assert(num === 5)
-    })
-  })
-  describe('power', () => {
-    test('value known at compiletime', () => {
-      const value = power(7, 4) // $ExpectType 2401
-      assert(value === 2401)
-    })
-    describe('value not known at compiletime', () => {
-      test('num', () => {
-        const value = power(2 as number, 4) // $ExpectType number
-        assert(value === 16)
-      })
-      test('power', () => {
-        const value = power(4, 8 as number) // $ExpectType number
-        assert(value === 65536)
-      })
-    })
-  })
 })
 
 describe('leadingZeros', () => {
-  test('values known at compiletime', () => {
-    const value = leadingZeros(12, 5) // $ExpectType "00012"
-    assert(value === '00012')
-  })
-  describe('values not known at compiletime', () => {
-    test('num', () => {
-      leadingZeros(12 as number, 5) // $ExpectType `${number}`
+    test('values known at compiletime', () => {
+        const value = leadingZeros(12, 5) // $ExpectType "00012"
+        assert(value === '00012')
     })
-    test('length', () => {
-      leadingZeros(12, 5 as number) // $ExpectType `${number}`
+    describe('values not known at compiletime', () => {
+        test('num', () => {
+            leadingZeros(12 as number, 5) // $ExpectType `${number}`
+        })
+        test('length', () => {
+            leadingZeros(12, 5 as number) // $ExpectType `${number}`
+        })
+        test('both', () => {
+            leadingZeros(12 as number, 5 as number) // $ExpectType `${number}`
+        })
     })
-    test('both', () => {
-      leadingZeros(12 as number, 5 as number) // $ExpectType `${number}`
+    test('negative numbers', () => {
+        const value = leadingZeros(-20, 4) // $ExpectType "-0020"
+        assert(value === '-0020')
     })
-  })
-  test('negative numbers', () => {
-    const value = leadingZeros(-20, 4) // $ExpectType "-0020"
-    assert(value === '-0020')
-  })
 })
 
 describe('ordinal', () => {
-  it('1/2/3', () => {
-    assert(ordinalNumber(1) === '1st')
-    assert(ordinalNumber(12) === '12th')
-    assert(ordinalNumber(23) === '23rd')
-    assert(
-      // @ts-expect-error wrong value
-      ordinalNumber(3) !== '3th',
-    )
-  })
-  it('th', () => {
-    assert(ordinalNumber(24) === '24th')
-    assert(ordinalNumber(0) === '0th')
-    assert(
-      // @ts-expect-error wrong value
-      ordinalNumber(0) !== '1000nd',
-    )
-  })
-  it('value not known at compiletime', () => {
-    ordinalNumber(1 as number) // $ExpectType `${number}st` | `${number}nd` | `${number}rd` | `${number}th`
-  })
+    it('1/2/3', () => {
+        assert(ordinalNumber(1) === '1st')
+        assert(ordinalNumber(12) === '12th')
+        assert(ordinalNumber(23) === '23rd')
+        assert(
+            // @ts-expect-error wrong value
+            ordinalNumber(3) !== '3th',
+        )
+    })
+    it('th', () => {
+        assert(ordinalNumber(24) === '24th')
+        assert(ordinalNumber(0) === '0th')
+        assert(
+            // @ts-expect-error wrong value
+            ordinalNumber(0) !== '1000nd',
+        )
+    })
+    it('value not known at compiletime', () => {
+        ordinalNumber(1 as number) // $ExpectType `${number}st` | `${number}nd` | `${number}rd` | `${number}th`
+    })
 })
 
 describe('comparison', () => {
-  describe('isGreaterThan', () => {
-    test('true', () => {
-      const value = isGreaterThan(5, 4) // $ExpectType true
-      assert(value)
+    describe('isGreaterThan', () => {
+        test('true', () => {
+            const value = isGreaterThan(5, 4) // $ExpectType true
+            assert(value)
+        })
+        test('false', () => {
+            isGreaterThan(5, 20) // $ExpectType false
+            const value = isGreaterThan(6, 6) // $ExpectType false
+            assert(!value)
+        })
+        test('big numbers', () => {
+            isGreaterThan(5000, 4999) // $ExpectType true
+            isGreaterThan(5000, 5001) // $ExpectType false
+        })
     })
-    test('false', () => {
-      isGreaterThan(5, 20) // $ExpectType false
-      const value = isGreaterThan(6, 6) // $ExpectType false
-      assert(!value)
-    })
-    test('big numbers', () => {
-      isGreaterThan(5000, 4999) // $ExpectType true
-      isGreaterThan(5000, 5001) // $ExpectType false
-    })
-  })
 })
 
 describe('bitwise operations', () => {
-  describe('leftShift', () => {
-    test('known at compile-time', () => {
-      const value = leftShift(5, 3) // $ExpectType 40
-      assert(value === 40)
+    describe('leftShift', () => {
+        test('known at compile-time', () => {
+            const value = leftShift(5, 3) // $ExpectType 40
+            assert(value === 40)
+        })
+        test('not known at compile-time', () => {
+            leftShift(5 as number, 3) // $ExpectType number
+        })
     })
-    test('not known at compile-time', () => {
-      leftShift(5 as number, 3) // $ExpectType number
+    describe('rightShift', () => {
+        test('known at compile-time', () => {
+            const value = rightShift(5, 2) // $ExpectType 1
+            assert(value === 1)
+        })
+        test('not known at compile-time', () => {
+            leftShift(5 as number, 2) // $ExpectType number
+        })
     })
-  })
-  describe('rightShift', () => {
-    test('known at compile-time', () => {
-      const value = rightShift(5, 2) // $ExpectType 1
-      assert(value === 1)
-    })
-    test('not known at compile-time', () => {
-      leftShift(5 as number, 2) // $ExpectType number
-    })
-  })
 })
--- src/utilityTypes/__tests__/String.spec.ts
+++ src/utilityTypes/__tests__/String.spec.ts
@@ -1,69 +1,69 @@
 import {
-  Email,
-  FileName,
-  IP,
-  LengthGreaterOrEqual,
-  LengthGreaterThan,
-  UriString,
-  UrlString,
-  CaseInsensitive,
+    Email,
+    FileName,
+    IP,
+    LengthGreaterOrEqual,
+    LengthGreaterThan,
+    UriString,
+    UrlString,
+    CaseInsensitive,
 } from '../String'
 import { testType } from '../../utilityFunctions/misc'
 
 test('UriString', () => {
-  // @ts-expect-error TS2345: Argument of type '"asdf"' is not assignable to parameter of type '`${string}://${string}`'.
-  testType<UriString>('asdf')
-  testType<UriString>('asdf://asdf.com')
-  // @ts-expect-error TS2345: Argument of type '"asdf://asdf.com"' is not assignable to parameter of type '`foo://${string}`'.
-  testType<UriString<'foo'>>('asdf://asdf.com')
+    // @ts-expect-error TS2345: Argument of type '"asdf"' is not assignable to parameter of type '`${string}://${string}`'.
+    testType<UriString>('asdf')
+    testType<UriString>('asdf://asdf.com')
+    // @ts-expect-error TS2345: Argument of type '"asdf://asdf.com"' is not assignable to parameter of type '`foo://${string}`'.
+    testType<UriString<'foo'>>('asdf://asdf.com')
 })
 test('UrlString', () => {
-  // @ts-expect-error TS2345: Argument of type '"asdf"' is not assignable to parameter of type '`${string}://${string}`'.
-  testType<UrlString>('asdf')
-  testType<UrlString>('https://asdf.com')
+    // @ts-expect-error TS2345: Argument of type '"asdf"' is not assignable to parameter of type '`${string}://${string}`'.
+    testType<UrlString>('asdf')
+    testType<UrlString>('https://asdf.com')
 })
 test('Email', () => {
-  // @ts-expect-error TS2345: Argument of type '"asdf.com"' is not assignable to parameter of type '`${string}@${string}.${string}`'.
-  testType<Email>('asdf.com')
-  // @ts-expect-error TS2345: Argument of type '"foo@asdf"' is not assignable to parameter of type '`${string}@${string}.${string}`'.
-  testType<Email>('foo@asdf')
-  testType<Email>('foo@bar.com')
+    // @ts-expect-error TS2345: Argument of type '"asdf.com"' is not assignable to parameter of type '`${string}@${string}.${string}`'.
+    testType<Email>('asdf.com')
+    // @ts-expect-error TS2345: Argument of type '"foo@asdf"' is not assignable to parameter of type '`${string}@${string}.${string}`'.
+    testType<Email>('foo@asdf')
+    testType<Email>('foo@bar.com')
 })
 
 test('IP', () => {
-  // @ts-expect-error invalid ip
-  testType<IP>('a.b.c.d')
-  // @ts-expect-error invalid ip (one too many)
-  testType<IP>('192.168.0.1.2')
-  testType<IP>('192.168.0.1')
+    // @ts-expect-error invalid ip
+    testType<IP>('a.b.c.d')
+    // @ts-expect-error invalid ip (one too many)
+    testType<IP>('192.168.0.1.2')
+    testType<IP>('192.168.0.1')
 })
 
 test('FileName', () => {
-  testType<FileName>('asdf')
-  testType<FileName<'png'>>('asdf.png')
-  // @ts-expect-error wrong filetype
-  testType<FileName<'png'>>('asdf.jpg')
+    testType<FileName>('asdf')
+    testType<FileName<'png'>>('asdf.png')
+    // @ts-expect-error wrong filetype
+    testType<FileName<'png'>>('asdf.jpg')
 })
 
 test('LengthGreaterOrEqual', () => {
-  testType<LengthGreaterOrEqual<'asdf', 4>>(true)
-  testType<LengthGreaterOrEqual<'asdf', 5>>(false)
-  testType<LengthGreaterOrEqual<'asdf', 3>>(true)
+    testType<LengthGreaterOrEqual<'asdf', 4>>(true)
+    testType<LengthGreaterOrEqual<'asdf', 5>>(false)
+    testType<LengthGreaterOrEqual<'asdf', 3>>(true)
 })
 
 test('LengthGreaterThan', () => {
-  testType<LengthGreaterThan<'asdf', 4>>(false)
-  testType<LengthGreaterThan<'asdf', 5>>(false)
-  testType<LengthGreaterThan<'asdf', 3>>(true)
+    testType<LengthGreaterThan<'asdf', 4>>(false)
+    testType<LengthGreaterThan<'asdf', 5>>(false)
+    testType<LengthGreaterThan<'asdf', 3>>(true)
 })
 
 test('CaseInsensitive', () => {
-  testType<CaseInsensitive<'abc'>>('abc')
-  testType<CaseInsensitive<'abc'>>('AbC')
-  testType<CaseInsensitive<'abc'>>('aBC')
-  testType<CaseInsensitive<'abc'>>('ABC')
-  testType<CaseInsensitive<'abc'>>('aBc')
-  testType<CaseInsensitive<'abc'>>('Abc')
-  // @ts-expect-error wrong value
-  testType<CaseInsensitive<'abc'>>('ABd')
+    testType<CaseInsensitive<'abc'>>('abc')
+    testType<CaseInsensitive<'abc'>>('AbC')
+    testType<CaseInsensitive<'abc'>>('aBC')
+    testType<CaseInsensitive<'abc'>>('ABC')
+    testType<CaseInsensitive<'abc'>>('aBc')
+    testType<CaseInsensitive<'abc'>>('Abc')
+    // @ts-expect-error wrong value
+    testType<CaseInsensitive<'abc'>>('ABd')
 })
