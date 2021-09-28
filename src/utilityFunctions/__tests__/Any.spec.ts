ea2cc954da9fd7c42347a575743e9b9f1058856e
detachhead <detachhead@users.noreply.github.com>
2021-09-28 14:29:30 +1000

    fix issue where hasPropertyPredicate was inferring a type where the value of the key is any

--- src/utilityFunctions/__tests__/Any.spec.ts
+++ src/utilityFunctions/__tests__/Any.spec.ts
@@ -1,4 +1,5 @@
-import { entries } from '../Any'
+import { entries, hasPropertyPredicate } from '../Any'
+import { exactly } from '../misc'
 import { PowerAssert } from 'typed-nodejs-assert'
 // eslint-disable-next-line @typescript-eslint/no-var-requires -- https://github.com/detachHead/typed-nodejs-assert#with-power-assert
 const assert: PowerAssert = require('power-assert')
@@ -9,4 +10,22 @@ test('entries', () => {
         ['foo', 1],
         ['bar', 'baz'],
     ])
+})
+
+describe('hasPropertyPredicate', () => {
+    const value = {} as unknown
+    describe('inferred', () => {
+        test('one value', () => {
+            if (hasPropertyPredicate(value, 'foo')) exactly<{ foo: unknown }>()(value)
+        })
+        test('union', () => {
+            if (hasPropertyPredicate(value, 'length' as 'length' | 'asdf')) exactly<{}>()(value)
+        })
+        test('not known at compiletime', () => {
+            if (hasPropertyPredicate(value, 'length' as string)) exactly<unknown>()(value)
+        })
+    })
+    test('explicit', () => {
+        if (hasPropertyPredicate<unknown[]>(value, 'length')) exactly<unknown[]>()(value)
+    })
 })
