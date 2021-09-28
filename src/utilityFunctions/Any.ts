f318875fca236208acb51af510a69db5bdb10946
detachhead <detachhead@users.noreply.github.com>
2021-09-28 14:29:30 +1000

    fix issue where hasPropertyPredicate was inferring a type where the value of the key is any

--- src/utilityFunctions/Any.ts
+++ src/utilityFunctions/Any.ts
@@ -1,9 +1,24 @@
-/**
- * returns whether the given object has the given propertyName and narrows the type of the object to the specified generic
- */
 import { Entries } from '../utilityTypes/Any'
+import { AnyKey } from 'tsdef'
+import { IntersectOf } from 'ts-toolbelt/out/Union/IntersectOf'
 
-export const hasPropertyPredicate = <T>(object: unknown, propertyName: keyof T): object is T =>
+/**
+ * returns whether the given object has the given propertyName and narrows the type of the object to either:
+ * - the specified generic (if provided)
+ * - OR adds the specified key to the type, if a generic is not provided
+ */
+export const hasPropertyPredicate: {
+    // this overload is only really needed because of https://github.com/microsoft/TypeScript/issues/46107
+    <Key extends AnyKey>(object: unknown, propertyName: Key): object is string extends Key
+        ? unknown
+        : {
+              // need an intersection of these types instead of a union, otherwise when Key is a union of two possible
+              //  properties it would incorrectly narrow to a type that has both
+              // @ts-expect-error TODO: figure out whats causing this, it seems to work just fine when suppressed
+              [K in IntersectOf<Key>]: unknown
+          }
+    <Narrowed>(object: unknown, propertyName: keyof Narrowed): object is Narrowed
+} = <T>(object: unknown, propertyName: keyof T): object is T =>
     Object.prototype.hasOwnProperty.call(object, propertyName)
 
 /**
