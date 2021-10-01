522643d4dce5559f6f318afa7a02112a65d1c257
DetachHead <detachhead@users.noreply.github.com>
2021-10-01 13:01:05 +1000

    add ipv4/ipv6 types & repurpose assertType function since there's still use cases for it

--- src/utilityTypes/__tests__/String.spec.ts
+++ src/utilityTypes/__tests__/String.spec.ts
@@ -1,69 +1,79 @@
 import {
     Email,
     FileName,
-    IP,
     LengthGreaterOrEqual,
     LengthGreaterThan,
     UriString,
     UrlString,
     CaseInsensitive,
+    IPv4,
+    IPv6,
 } from '../String'
-import { testType } from '../../utilityFunctions/misc'
+import { assertType } from '../../utilityFunctions/misc'
 
 test('UriString', () => {
     // @ts-expect-error TS2345: Argument of type '"asdf"' is not assignable to parameter of type '`${string}://${string}`'.
-    testType<UriString>('asdf')
-    testType<UriString>('asdf://asdf.com')
+    assertType<UriString>('asdf')
+    assertType<UriString>('asdf://asdf.com')
     // @ts-expect-error TS2345: Argument of type '"asdf://asdf.com"' is not assignable to parameter of type '`foo://${string}`'.
-    testType<UriString<'foo'>>('asdf://asdf.com')
+    assertType<UriString<'foo'>>('asdf://asdf.com')
 })
 test('UrlString', () => {
     // @ts-expect-error TS2345: Argument of type '"asdf"' is not assignable to parameter of type '`${string}://${string}`'.
-    testType<UrlString>('asdf')
-    testType<UrlString>('https://asdf.com')
+    assertType<UrlString>('asdf')
+    assertType<UrlString>('https://asdf.com')
 })
 test('Email', () => {
     // @ts-expect-error TS2345: Argument of type '"asdf.com"' is not assignable to parameter of type '`${string}@${string}.${string}`'.
-    testType<Email>('asdf.com')
+    assertType<Email>('asdf.com')
     // @ts-expect-error TS2345: Argument of type '"foo@asdf"' is not assignable to parameter of type '`${string}@${string}.${string}`'.
-    testType<Email>('foo@asdf')
-    testType<Email>('foo@bar.com')
+    assertType<Email>('foo@asdf')
+    assertType<Email>('foo@bar.com')
 })
 
-test('IP', () => {
-    // @ts-expect-error invalid ip
-    testType<IP>('a.b.c.d')
-    // @ts-expect-error invalid ip (one too many)
-    testType<IP>('192.168.0.1.2')
-    testType<IP>('192.168.0.1')
+describe('IP', () => {
+    test('v4', () => {
+        // @ts-expect-error invalid ip
+        assertType<IPv4>('a.b.c.d')
+        // @ts-expect-error invalid ip (one too many)
+        assertType<IPv4>('192.168.0.1.2')
+        assertType<IPv4>('192.168.0.1')
+    })
+    test('v6', () => {
+        // @ts-expect-error invalid ip
+        assertType<IPv6>('asdf')
+        assertType<IPv6>('2001:0db8:0000:0000:0000:ff00:0042:8329')
+        // @ts-expect-error invalid ip (not enough)
+        assertType<IPv6>('2001:0db8:0000:0000:0000:ff00:0042')
+    })
 })
 
 test('FileName', () => {
-    testType<FileName>('asdf')
-    testType<FileName<'png'>>('asdf.png')
+    assertType<FileName>('asdf')
+    assertType<FileName<'png'>>('asdf.png')
     // @ts-expect-error wrong filetype
-    testType<FileName<'png'>>('asdf.jpg')
+    assertType<FileName<'png'>>('asdf.jpg')
 })
 
 test('LengthGreaterOrEqual', () => {
-    testType<LengthGreaterOrEqual<'asdf', 4>>(true)
-    testType<LengthGreaterOrEqual<'asdf', 5>>(false)
-    testType<LengthGreaterOrEqual<'asdf', 3>>(true)
+    assertType<LengthGreaterOrEqual<'asdf', 4>>(true)
+    assertType<LengthGreaterOrEqual<'asdf', 5>>(false)
+    assertType<LengthGreaterOrEqual<'asdf', 3>>(true)
 })
 
 test('LengthGreaterThan', () => {
-    testType<LengthGreaterThan<'asdf', 4>>(false)
-    testType<LengthGreaterThan<'asdf', 5>>(false)
-    testType<LengthGreaterThan<'asdf', 3>>(true)
+    assertType<LengthGreaterThan<'asdf', 4>>(false)
+    assertType<LengthGreaterThan<'asdf', 5>>(false)
+    assertType<LengthGreaterThan<'asdf', 3>>(true)
 })
 
 test('CaseInsensitive', () => {
-    testType<CaseInsensitive<'abc'>>('abc')
-    testType<CaseInsensitive<'abc'>>('AbC')
-    testType<CaseInsensitive<'abc'>>('aBC')
-    testType<CaseInsensitive<'abc'>>('ABC')
-    testType<CaseInsensitive<'abc'>>('aBc')
-    testType<CaseInsensitive<'abc'>>('Abc')
+    assertType<CaseInsensitive<'abc'>>('abc')
+    assertType<CaseInsensitive<'abc'>>('AbC')
+    assertType<CaseInsensitive<'abc'>>('aBC')
+    assertType<CaseInsensitive<'abc'>>('ABC')
+    assertType<CaseInsensitive<'abc'>>('aBc')
+    assertType<CaseInsensitive<'abc'>>('Abc')
     // @ts-expect-error wrong value
-    testType<CaseInsensitive<'abc'>>('ABd')
+    assertType<CaseInsensitive<'abc'>>('ABd')
 })
