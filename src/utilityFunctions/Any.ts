c170a5f16db77b7c8dc64a4c1cd9d102684d200a
DetachHead <detachhead@users.noreply.github.com>
2021-10-04 23:31:12 +1000

    update hasPropertyPredicate documentation

--- src/utilityFunctions/Any.ts
+++ src/utilityFunctions/Any.ts
@@ -8,15 +8,22 @@ import { IntersectOf } from 'ts-toolbelt/out/Union/IntersectOf'
  * - OR adds the specified key to the type, if a generic is not provided
  */
 export const hasPropertyPredicate: {
+    /**
+     * returns whether the given object has the given propertyName and narrows the type of the object to add the
+     * specified key to the type
+     */
     // this overload is only really needed because of https://github.com/microsoft/TypeScript/issues/46107
     <Key extends AnyKey>(object: unknown, propertyName: Key): object is string extends Key
         ? unknown
         : {
               // need an intersection of these types instead of a union, otherwise when Key is a union of two possible
               //  properties it would incorrectly narrow to a type that has both
-              // @ts-expect-error TODO: figure out whats causing this, it seems to work just fine when suppressed
+              // @ts-expect-error https://github.com/microsoft/TypeScript/issues/46176#issuecomment-933422434
               [K in IntersectOf<Key>]: unknown
           }
+    /**
+     * returns whether the given object has the given propertyName and narrows the type of the object to the specified generic
+     */
     <Narrowed>(object: unknown, propertyName: keyof Narrowed): object is Narrowed
 } = <T>(object: unknown, propertyName: keyof T): object is T =>
     Object.prototype.hasOwnProperty.call(object, propertyName)
