465759b2afd2c5e77fcca05ab58e6718631e8a6e
DetachHead <detachhead@users.noreply.github.com>
2021-10-04 18:23:46 +1000

    rewrite EvaluateType to take an optional depth parameter

--- src/utilityFunctions/__tests__/Any.spec.ts
+++ src/utilityFunctions/__tests__/Any.spec.ts
@@ -1,6 +1,7 @@
 import { entries, hasPropertyPredicate } from '../Any'
 import { exactly } from '../misc'
 import { PowerAssert } from 'typed-nodejs-assert'
+import { EvaluateType, Keys } from '../../utilityTypes/Any'
 // eslint-disable-next-line @typescript-eslint/no-var-requires -- https://github.com/detachHead/typed-nodejs-assert#with-power-assert
 const assert: PowerAssert = require('power-assert')
 
@@ -28,4 +29,14 @@ describe('hasPropertyPredicate', () => {
     test('explicit', () => {
         if (hasPropertyPredicate<unknown[]>(value, 'length')) exactly<unknown[]>()(value)
     })
+})
+
+test('EvaluateType', () => {
+    type Entries<T> = EvaluateType<[Keys<T>, T[Keys<T>]][]>
+    const value = {} as Entries<{ foo: number; bar: string }>
+    // want to remove usages of ExpectType in most places because the exactly function is better as it actually checks
+    // the structure of the types rather than how the compiler represents them. however in this case that's what we want
+    // see https://github.com/JoshuaKGoldberg/eslint-plugin-expect-type/issues/18
+    // noinspection BadExpressionStatementJS
+    value // $ExpectType ["foo" | "bar", string | number][]
 })
--- src/utilityTypes/Any.ts
+++ src/utilityTypes/Any.ts
@@ -1,4 +1,5 @@
 import { Keys as TsToolbeltKeys } from 'ts-toolbelt/out/Any/Keys'
+import { Decrement } from './Number'
 
 /**
  * makes typescript evaluate the result of a type when you hover over it
@@ -12,17 +13,16 @@ import { Keys as TsToolbeltKeys } from 'ts-toolbelt/out/Any/Keys'
  * declare const foo: Entries<{ foo: number, bar: string }> //hovering over foo will show `[("foo" | "bar"), (string | number)][]`
  * @see https://stackoverflow.com/a/57683652
  */
-export type EvaluateType<T> = T extends infer O ? { [K in keyof O]: O[K] } : never
-
-/**
- * {@link EvaluateType} but works recursively on objects
- * @see https://stackoverflow.com/a/57683652
- */
-export type EvaluateTypeRecursively<T> = T extends object
-    ? T extends infer O
-        ? { [K in keyof O]: EvaluateTypeRecursively<O[K]> }
+// TODO: figure out why this doesnt work on everything, eg. TupleOfUpTo<number, 3>
+export type EvaluateType<T, Depth extends number = 1> = T extends object
+    ? Depth extends 0
+        ? T
+        : T extends infer O
+        ? { [K in keyof O]: EvaluateType<O[K], Decrement<Depth>> }
         : never
-    : T
+    : T extends infer O
+    ? { [K in keyof O]: O[K] }
+    : never
 
 /** like ts-toolbelt's `Keys` except it doesn't include number (for objects where you know all of the keys) */
 export type Keys<T> = Exclude<TsToolbeltKeys<T>, number>
