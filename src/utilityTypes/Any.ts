216dc684c9521b0b889b7036847d4b701833b43c
DetachHead <detachhead@users.noreply.github.com>
2021-10-11 08:19:28 +1000

    update documentation for Any/Keys to explain the difference between ts-toolbelt's KnownKeys

--- src/utilityTypes/Any.ts
+++ src/utilityTypes/Any.ts
@@ -1,6 +1,10 @@
 import { Keys as TsToolbeltKeys } from 'ts-toolbelt/out/Any/Keys'
 
-/** like ts-toolbelt's `Keys` except it doesn't include number (for objects where you know all of the keys) */
+/**
+ * like ts-toolbelt's `Keys` except it doesn't include number (for objects where you know all of the keys)
+ *
+ * in some cases, `ts-toolbelt`'s `KnownKeys` will probably work for you, but it doesn't seem to work properly for arrays
+ */
 export type Keys<T> = Exclude<TsToolbeltKeys<T>, number>
 
 /** compiletime version of {@link ObjectConstructor.entries} */
