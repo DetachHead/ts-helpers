827da4eebd29879bd286daf179ec541f192f7ac1
DetachHead <detachhead@users.noreply.github.com>
2021-10-10 23:35:49 +1000

    remove EvaluateType because it doesnt work properly and ts-toolbelt has Compute which actually works

--- src/utilityFunctions/__tests__/Any.spec.ts
+++ src/utilityFunctions/__tests__/Any.spec.ts
@@ -1,16 +1,14 @@
 import { entries, hasPropertyPredicate } from '../Any'
 import { exactly } from '../misc'
-import { PowerAssert } from 'typed-nodejs-assert'
-import { EvaluateType, Keys } from '../../utilityTypes/Any'
-// eslint-disable-next-line @typescript-eslint/no-var-requires -- https://github.com/detachHead/typed-nodejs-assert#with-power-assert
-const assert: PowerAssert = require('power-assert')
 
 test('entries', () => {
-    const value = entries({ foo: 1, bar: 'baz' }) // $ExpectType ["foo" | "bar", string | number][]
-    assert.deepStrictEqual(value, [
-        ['foo', 1],
-        ['bar', 'baz'],
-    ])
+    exactly(
+        [
+            ['foo', 1],
+            ['bar', 'baz'],
+        ] as ['foo' | 'bar', string | number][],
+        entries({ foo: 1, bar: 'baz' }),
+    )
 })
 
 describe('hasPropertyPredicate', () => {
@@ -29,14 +27,4 @@ describe('hasPropertyPredicate', () => {
     test('explicit', () => {
         if (hasPropertyPredicate<unknown[]>(value, 'length')) exactly<unknown[]>()(value)
     })
-})
-
-test('EvaluateType', () => {
-    type Entries<T> = EvaluateType<[Keys<T>, T[Keys<T>]][]>
-    const value = {} as Entries<{ foo: number; bar: string }>
-    // want to remove usages of ExpectType in most places because the exactly function is better as it actually checks
-    //  the structure of the types rather than how the compiler represents them. however in this case that's what we want
-    //  see https://github.com/JoshuaKGoldberg/eslint-plugin-expect-type/issues/18
-    // noinspection BadExpressionStatementJS
-    value // $ExpectType ["foo" | "bar", string | number][]
 })
