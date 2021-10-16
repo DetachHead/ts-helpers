75c06d76d8607f01814a28e0489e8e22a0e226e7
DetachHead <detachhead@users.noreply.github.com>
2021-10-16 11:04:50 +1000

    fix ts-jest types

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,6 @@
-import { Config } from '@jest/types'
+import type { InitialOptionsTsJest } from 'ts-jest/dist/types'
 
-const config: Config.InitialOptions = {
+const config: InitialOptionsTsJest = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))[^d]\\.ts$',
