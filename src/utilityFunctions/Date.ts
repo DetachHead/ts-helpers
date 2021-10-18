edf1eac25ec97eb871e70992ed2b7ce2728b3158
DetachHead <detachhead@users.noreply.github.com>
2021-10-19 00:28:57 +1000

    remove wrong comment

--- src/utilityFunctions/Date.ts
+++ src/utilityFunctions/Date.ts
@@ -5,7 +5,6 @@ import { format } from 'date-fns'
  * formats a date using the [`date-fns` format](https://date-fns.org/v2.20.1/docs/format) function, and uses
  * {@link FormattedDate} to generate a type for the given `formatString`
  */
-// explicit type annotation is required here or else the expect-type exlint rule complains
 export const formatDate: <T extends string>(date: Date, formatString: T) => FormattedDate<T> = (
     date,
     formatString,
