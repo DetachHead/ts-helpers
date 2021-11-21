4f462a7855bc0d3031758e5fc494bea52ec9e24a
DetachHead <detachhead@users.noreply.github.com>
2021-11-21 22:30:30 +1000

    add `ReplaceRecursive` type

--- src/utilityTypes/Any.ts
+++ src/utilityTypes/Any.ts
@@ -1,4 +1,5 @@
 import { Keys as TsToolbeltKeys } from 'ts-toolbelt/out/Any/Keys'
+import { AnyFunction } from 'tsdef'
 
 /**
  * like ts-toolbelt's `Keys` except it doesn't include number (for objects where you know all of the keys)
@@ -9,3 +10,23 @@ export type Keys<T> = Exclude<TsToolbeltKeys<T>, number>
 
 /** compiletime version of {@link ObjectConstructor.entries} */
 export type Entries<T> = [Keys<T>, T[Keys<T>]][]
+
+/**
+ * Replace in `T` those types that are assignable to `Find` with the types that are assignable to `ReplaceWith`
+ */
+export type Replace<T, Find, ReplaceWith> = Exclude<T, Find> | ReplaceWith
+
+export type ReplaceRecursive<T, Find, ReplaceWith> = T extends object
+    ? {
+          [K in keyof T]: ReplaceRecursive<T[K], Find, ReplaceWith>
+      } &
+          T extends AnyFunction
+        ? (
+              ...args: ReplaceRecursive<Parameters<T & AnyFunction>, Find, ReplaceWith> & unknown[]
+          ) => ReplaceRecursive<ReturnType<T & AnyFunction>, Find, ReplaceWith>
+        : unknown & T extends readonly unknown[]
+        ? readonly unknown[]
+        : T extends unknown[]
+        ? unknown[]
+        : unknown
+    : Replace<T, Find, ReplaceWith>
