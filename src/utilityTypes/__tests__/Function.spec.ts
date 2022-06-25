8c0f90a7abb8ecab3f581291b6c197c1a3ba19b6
DetachHead <detachhead@users.noreply.github.com>
2022-06-26 01:12:10 +1000

    half busted attempt at fixing private methods with variance types

--- src/utilityTypes/__tests__/Function.spec.ts
+++ src/utilityTypes/__tests__/Function.spec.ts
@@ -9,6 +9,7 @@ declare class ContravariantToArrowFunctionTest<T> {
 }
 
 test('ToArrowFunction', () => {
+    assertType<BivariantToArrowFunctionTest<unknown>, ContravariantToArrowFunctionTest<unknown>>()
     assertType<BivariantToArrowFunctionTest<unknown>, BivariantToArrowFunctionTest<number>>()
     assertType<BivariantToArrowFunctionTest<number>, BivariantToArrowFunctionTest<unknown>>()
     assertType<
@@ -27,6 +28,10 @@ declare class ContravariantToNonArrowFunctionTest<T> {
 
 test('ToNonArrowFunction', () => {
     assertType<
+        BivariantToNonArrowFunctionTest<unknown>,
+        ContravariantToNonArrowFunctionTest<unknown>
+    >()
+    assertType<
         ContravariantToNonArrowFunctionTest<unknown>,
         ContravariantToNonArrowFunctionTest<number>
     >()
@@ -41,18 +46,56 @@ test('ToNonArrowFunction', () => {
     >()
 })
 
-test('SafeVariance', () => {
-    type Contravariant<T> = SafeVariance<BivariantToArrowFunctionTest<T>>
-    assertType<Contravariant<number>, Contravariant<unknown>>()
-    assertType<
-        Contravariant<unknown>,
-        // @ts-expect-error -- negative test
-        Contravariant<number>
-    >()
+declare class SafeVarianceWithPrivateMethodTest<T> {
+    foo(_value: T): void
+    private bar(_value: T): void
+    private baz: number
+}
+
+describe('SafeVariance', () => {
+    test('normal', () => {
+        type Contravariant<T> = SafeVariance<BivariantToArrowFunctionTest<T>>
+        assertType<BivariantToArrowFunctionTest<unknown>, Contravariant<unknown>>()
+        assertType<Contravariant<number>, Contravariant<unknown>>()
+        assertType<
+            Contravariant<unknown>,
+            // @ts-expect-error -- negative test
+            Contravariant<number>
+        >()
+    })
+    test('private methods', () => {
+        type Contravariant<T> = SafeVariance<SafeVarianceWithPrivateMethodTest<T>>
+        assertType<SafeVarianceWithPrivateMethodTest<unknown>, Contravariant<unknown>>()
+        assertType<Contravariant<number>, Contravariant<unknown>>()
+        assertType<
+            Contravariant<unknown>,
+            // @ts-expect-error -- negative test
+            Contravariant<number>
+        >()
+    })
 })
 
-test('UnsafeVariance', () => {
-    type Bivariant<T> = UnsafeVariance<BivariantToNonArrowFunctionTest<T>>
-    assertType<Bivariant<number>, Bivariant<unknown>>()
-    assertType<Bivariant<unknown>, Bivariant<number>>()
+declare class UnsafeVarianceWithPrivateMethodTest<T> {
+    foo: (_value: T) => void
+    private bar: (_value: T) => void
+    private baz: number
+}
+
+describe('UnsafeVariance', () => {
+    test('normal', () => {
+        type Bivariant<T> = UnsafeVariance<BivariantToNonArrowFunctionTest<T>>
+        assertType<BivariantToNonArrowFunctionTest<unknown>, Bivariant<unknown>>()
+        assertType<Bivariant<number>, Bivariant<unknown>>()
+        assertType<Bivariant<unknown>, Bivariant<number>>()
+    })
+    test('private methods', () => {
+        type Bivariant<T> = UnsafeVariance<UnsafeVarianceWithPrivateMethodTest<T>>
+        assertType<
+            UnsafeVarianceWithPrivateMethodTest<unknown>,
+            // @ts-expect-error xfail
+            Bivariant<unknown>
+        >()
+        assertType<Bivariant<number>, Bivariant<unknown>>()
+        assertType<Bivariant<unknown>, Bivariant<number>>()
+    })
 })
