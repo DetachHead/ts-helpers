76ac387f6f4a3324d39b36a5373e2a16deada3d5
detachhead <detachhead@users.noreply.github.com>
2023-02-28 16:01:30 +1000

    fix `Intersection` type with unions and add `CheckNever` type

--- test/types/misc.spec.ts
+++ test/types/misc.spec.ts
@@ -1,5 +1,11 @@
 import { assertType, exactly } from '../../src/functions/misc'
-import { Intersection, IsExactOptionalProperty, NoAny, OnlyInfer } from '../../src/types/misc'
+import {
+    CheckNever,
+    Intersection,
+    IsExactOptionalProperty,
+    NoAny,
+    OnlyInfer,
+} from '../../src/types/misc'
 
 describe('OnlyInfer', () => {
     test('basic', () => {
@@ -35,51 +41,180 @@ describe('IsExactOptionalProperty', () => {
     })
 })
 
+describe('CheckNever', () => {
+    test('yes never', () => {
+        exactly<never, CheckNever<number & { a: string }>>()
+        exactly<never, CheckNever<number & string>>()
+    })
+    test('no never', () => {
+        exactly<{ a: string }, CheckNever<{ a: string }>>()
+        exactly<string, CheckNever<string>>()
+    })
+    test('union', () => {
+        exactly<
+            { a: string } & { b: number },
+            CheckNever<({ a: string } & { b: number }) | ({ a: string } & string)>
+        >()
+        exactly<string, CheckNever<string>>()
+    })
+})
+
 // TODO: figure out why these don't work with exactly type
 describe('Intersection', () => {
     describe('key type and index signature', () => {
-        test('top level', () => {
-            interface Foo {
-                a: string
-            }
-            interface Bar {
-                [key: string]: 'foo'
-            }
-            assertType<'foo', Intersection<Foo, Bar>['a']>()
-            assertType<'foo', Intersection<Bar, Foo>['a']>()
-            assertType<
-                'asdf',
-                // @ts-expect-error negative test
-                Intersection<Foo, Bar>['a']
-            >()
-            assertType<
-                'asdf',
-                // @ts-expect-error negative test
-                Intersection<Bar, Foo>['a']
-            >()
-        })
-        test('nested', () => {
-            interface Foo {
-                a: {
+        describe('top level', () => {
+            describe('index access type is narrower', () => {
+                interface Foo {
                     a: string
                 }
-            }
+                interface Bar {
+                    [key: string]: 'foo'
+                }
+                test('normal', () => {
+                    assertType<'foo', Intersection<Foo, Bar>['a']>()
+                    assertType<'foo', Intersection<Bar, Foo>['a']>()
+                    assertType<
+                        'asdf',
+                        // @ts-expect-error negative test
+                        Intersection<Foo, Bar>['a']
+                    >()
+                    assertType<
+                        'asdf',
+                        // @ts-expect-error negative test
+                        Intersection<Bar, Foo>['a']
+                    >()
+                })
+                test('distributive', () => {
+                    assertType<'foo', Intersection<Foo | number, Bar>['a']>()
+                    assertType<'foo', Intersection<Bar, Foo | number>['a']>()
+                    assertType<
+                        'asdf',
+                        // @ts-expect-error negative test
+                        Intersection<Foo | number, Bar>['a']
+                    >()
+                    assertType<
+                        'asdf',
+                        // @ts-expect-error negative test
+                        Intersection<Bar, Foo | number>['a']
+                    >()
+                })
+            })
+            describe('index access type is wider', () => {
+                interface Foo {
+                    a: 'foo'
+                }
+                interface Bar {
+                    [key: string]: string
+                }
+                test('normal', () => {
+                    assertType<'foo', Intersection<Foo, Bar>['a']>()
+                    assertType<'foo', Intersection<Bar, Foo>['a']>()
+                    assertType<
+                        'asdf',
+                        // @ts-expect-error negative test
+                        Intersection<Foo, Bar>['a']
+                    >()
+                    assertType<
+                        'asdf',
+                        // @ts-expect-error negative test
+                        Intersection<Bar, Foo>['a']
+                    >()
+                })
+                test('distributive', () => {
+                    assertType<'foo', Intersection<Foo | number, Bar>['a']>()
+                    assertType<'foo', Intersection<Bar, Foo | number>['a']>()
+                    assertType<
+                        'asdf',
+                        // @ts-expect-error negative test
+                        Intersection<Foo | number, Bar>['a']
+                    >()
+                    assertType<
+                        'asdf',
+                        // @ts-expect-error negative test
+                        Intersection<Bar, Foo | number>['a']
+                    >()
+                })
+            })
+        })
+        describe('nested', () => {
+            describe('index access type is narrower', () => {
+                interface Foo {
+                    a: {
+                        a: string
+                    }
+                }
 
-            interface Bar {
-                a: { [key: string]: 'foo' }
-            }
-            assertType<'foo', Intersection<Foo, Bar>['a']['a']>()
-            assertType<'foo', Intersection<Bar, Foo>['a']['a']>()
-            assertType<
-                'asdf',
-                // @ts-expect-error negative test
-                Intersection<Foo, Bar>['a']['a']
-            >()
-            assertType<
-                'asdf',
-                // @ts-expect-error negative test
-                Intersection<Bar, Foo>['a']['a']
-            >()
+                interface Bar {
+                    a: { [key: string]: 'foo' }
+                }
+                test('normal', () => {
+                    assertType<'foo', Intersection<Foo, Bar>['a']['a']>()
+                    assertType<'foo', Intersection<Bar, Foo>['a']['a']>()
+                    assertType<
+                        'asdf',
+                        // @ts-expect-error negative test
+                        Intersection<Foo, Bar>['a']['a']
+                    >()
+                    assertType<
+                        'asdf',
+                        // @ts-expect-error negative test
+                        Intersection<Bar, Foo>['a']['a']
+                    >()
+                })
+                test('distributive', () => {
+                    assertType<'foo', Intersection<Foo | number, Bar>['a']['a']>()
+                    assertType<'foo', Intersection<Bar, Foo | number>['a']['a']>()
+                    assertType<
+                        'asdf',
+                        // @ts-expect-error negative test
+                        Intersection<Foo | number, Bar>['a']['a']
+                    >()
+                    assertType<
+                        'asdf',
+                        // @ts-expect-error negative test
+                        Intersection<Bar, Foo | number>['a']['a']
+                    >()
+                })
+            })
+            describe('index access type is wider', () => {
+                interface Foo {
+                    a: {
+                        a: 'foo'
+                    }
+                }
+
+                interface Bar {
+                    a: { [key: string]: string }
+                }
+                test('normal', () => {
+                    assertType<'foo', Intersection<Foo, Bar>['a']['a']>()
+                    assertType<'foo', Intersection<Bar, Foo>['a']['a']>()
+                    assertType<
+                        'asdf',
+                        // @ts-expect-error negative test
+                        Intersection<Foo, Bar>['a']['a']
+                    >()
+                    assertType<
+                        'asdf',
+                        // @ts-expect-error negative test
+                        Intersection<Bar, Foo>['a']['a']
+                    >()
+                })
+                test('distributive', () => {
+                    assertType<'foo', Intersection<Foo | number, Bar>['a']['a']>()
+                    assertType<'foo', Intersection<Bar, Foo | number>['a']['a']>()
+                    assertType<
+                        'asdf',
+                        // @ts-expect-error negative test
+                        Intersection<Foo | number, Bar>['a']['a']
+                    >()
+                    assertType<
+                        'asdf',
+                        // @ts-expect-error negative test
+                        Intersection<Bar, Foo | number>['a']['a']
+                    >()
+                })
+            })
         })
     })
     describe('normal intersections', () => {
