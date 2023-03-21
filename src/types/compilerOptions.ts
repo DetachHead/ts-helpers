eb3afe89fce34679304ae04c8b137a4df28fb04b
detachhead <detachhead@users.noreply.github.com>
2023-03-21 10:25:42 +1000

    split `_internal` into two separate files so only the `noUncheckedIndexedAccess` checker needs to be a `.d.ts` file and prettier-ignore it due to syntax error

--- src/types/compilerOptions.ts
+++ src/types/compilerOptions.ts
@@ -3,10 +3,12 @@ types for checking whether specific compiler options are enabled. useful when cr
 depending on specific settings in `tsconfig.json`
 */
 // this import needs src in it or it won't work when compiled
-import { indexedAccessCheck } from '../../src/types/_internal'
+import { noUncheckedIndexedAccessChecker } from '../../src/types/_internal/noUncheckedIndexedAccessChecker'
 
 /** whether or not `noUncheckedIndexedAccess` is enabled in this project */
-export type NoUncheckedIndexedAccess = undefined extends typeof indexedAccessCheck ? true : false
+export type NoUncheckedIndexedAccess = undefined extends typeof noUncheckedIndexedAccessChecker
+    ? true
+    : false
 
 /** whether or not `exactOptionalPropertyTypes` is enabled in this project */
 export type ExactOptionalPropertyTypes = undefined extends Required<{
